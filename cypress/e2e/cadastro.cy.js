7ff910102660a24523b8d3451da0fe1f89defeb6
Kelly Martins <kellyrdrmartins@gmail.com>
2023-03-14 10:58:47 -0300

    duvidas

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -9,7 +9,15 @@ describe('Cadastro ', () => {
     cy.visit('/minha-conta')
   })
   it('Deve realizar o cadastro na EBAC-Shop ', () => {
-    cadastroPage.register(email, dados.senha)
+    cy.intercept('POST', '/minha-conta').as('novo-cadastro')
+    cy.get('#reg_email').type(email)
+    cy.get('#reg_password').type(dados.senha)
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.wait('@novo-cadastro').its('response.statusCode').should('eq', 302)
+    cy.get('@novo-cadastro').then(res => {
+      expect(res.response.body).to.eq('')
+    })
     minhaContaPage.message.should(
       'contain',
       'A partir do painel de controle de sua conta'
