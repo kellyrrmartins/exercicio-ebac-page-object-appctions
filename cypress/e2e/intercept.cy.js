19101f067dad7eddc42cf3bb7a36e379bda53e45
Kelly Martins <kellyrdrmartins@gmail.com>
2023-03-17 10:39:34 -0300

    update intercept

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -2,6 +2,7 @@
 
 // import { HomePage } from '../support/pages'
 const produtos = require('../fixtures/produtos.json')
+const fragmento = require('../fixtures/fragmento.json')
 
 describe('Carrinho', () => {
   beforeEach(() => {
@@ -9,18 +10,19 @@ describe('Carrinho', () => {
   })
 
   it('Deve adicionar um produto com sucesso', () => {
-    cy.intercept('POST', '/product/augusta-pullover-jacket').as('novo-produto')
+    cy.intercept('POST', '/product/augusta-pullover-jacket', {
+      fixture: 'resposta.html'
+    }).as('novo-produto')
     cy.get('[class="product-block grid"]').contains(produtos.nome).click()
     cy.get('.button-variable-item-' + produtos.tamanho).click()
     cy.get('.button-variable-item-' + produtos.cor).click()
     cy.get('.input-text').clear().type(produtos.quantidade)
     cy.get('.single_add_to_cart_button').click()
 
-    // cy.wait('@novo-produto').then(resp => {
-    //   expect(resp.response.statusCode).to.equal(200)
-    //   expect(resp.response.statusMessage).to.equal('OK')
-    //   cy.log(resp)
-    // })
+    cy.get('@novo-produto').then(resp => {
+      expect(resp.response.statusCode).to.equal(200)
+      cy.log(resp)
+    })
     cy.get('@novo-produto').then(novoProduto => {
       cy.log(novoProduto)
     })
@@ -28,14 +30,36 @@ describe('Carrinho', () => {
   it('Teste com Intercept', () => {
     cy.intercept('POST', '/product/augusta-pullover-jacket', {
       fixture: 'resposta.html'
-    }).as('getNovo-Produto')
-    cy.intercept('POST', '/product/augusta-pullover-jacket').as('novo-produto')
+    }).as('novo-produto')
+
     cy.get('[class="product-block grid"]').contains(produtos.nome).click()
     cy.get('.button-variable-item-' + produtos.tamanho).click()
     cy.get('.button-variable-item-' + produtos.cor).click()
     cy.get('.input-text').clear().type(produtos.quantidade)
     cy.get('.single_add_to_cart_button').click()
+    cy.get('@novo-produto').then(res => {
+      expect(res.response.statusCode).to.equal(200)
 
-    cy.visit('/produtos/page/2')
+      cy.log(res)
+    })
+  })
+  it.only('Pegar o produto do carrinho', () => {
+    cy.intercept('POST', '/product/augusta-pullover-jacket', {
+      fixture: 'resposta.html'
+    }).as('novo-produto')
+    cy.intercept('POST', '/?wc-ajax=get_refreshed_fragments', {
+      fixture: 'intercept.json'
+    }).as('getCarrinho')
+    cy.get('[class="product-block grid"]').contains(produtos.nome).click()
+    cy.get('.button-variable-item-' + produtos.tamanho).click()
+    cy.get('.button-variable-item-' + produtos.cor).click()
+    cy.get('.input-text').clear().type(produtos.quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.intercept('POST', '/wp-admin/admin-ajax.php', req => {}).as(
+      'getCarrinho'
+    )
+    cy.get('@getCarrinho').then(resp => {
+      cy.log(resp)
+    })
   })
 })
