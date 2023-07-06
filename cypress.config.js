cb2cbbd4579682cda55eec17d118faf2b2d79949
Kelly Martins <kellyrdrmartins@gmail.com>
2023-07-06 10:47:43 -0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) { },
     baseUrl: 'http://lojaebac.ebaconline.art.br',
     reporter: 'mochawesome',
     reporterOptions: {
